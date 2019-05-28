63fcf779b6bd52f9a4685f31176f15549f9cace2
Prof Game <kadomony@gmail.com>
2019-05-28 12:16:51 +0200

    Cubes that alternate colors

---
+++ Game4-Memory/Assets/Scripts/MemoryCube.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCube : MonoBehaviour {
+
+    public Color baseColor;
+    public Color secondaryColor;
+
+    private Renderer render;
+
+    // Use this for initialization
+	void Start () {
+        render = GetComponent<Renderer>();
+        render.material.color = baseColor;
+
+    }
+
+    private void OnMouseDown() {
+        if (render.material.color == baseColor) {
+            render.material.color = secondaryColor;
+        } else {
+            render.material.color = baseColor;
+        }
+    }
+
+}
